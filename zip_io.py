eb06d7be8daa9243931e4468d0909df65d1677ba
David Thaler <davidthaler@gmail.com>
2015-09-22 18:01:42 -0700

    changed zip_io generator to include labels or file names

--- zip_io.py
+++ zip_io.py
@@ -5,29 +5,40 @@ import util
 
 ZIP_TEMPLATE = '%s.zip'
 
-# TODO: get a better name for this...its a generator.
-def read_archive(archive_num, whitelist=None):
+      
+def one_archive(archive_num, train_data):
   '''
-  A generator that produces BeautifulSoup objects (parsed web pages)
+  A generator that produces tuples of (label or filename, soup), 
+  where soup is a web page parsed by BeautifulSoup,
   for all of the files in an archive.
   Assumes the naming convention: 0.zip...5.zip
   
   Args:
     archive_num - either an int or string 0...5 or '0'...'5'
-    whitelist - A set or none, default None. 
-        If not None, only use files with names in the whitelist.
+    train_data - A dict or none. 
+      If dict, it maps filenames to labels, and labels are included in output.
+      If None, file names are included in output.
         
   Generate:
-    A BeautifulSoup object for each file in the zip archive.
+    A tuple of either labels or file names and a BeautifulSoup object 
+    for each file in the zip archive.
   '''
-  filename = ZIP_TEMPLATE % str(archive_num)
-  archive_path = os.path.join(util.DATA, filename)
+  archive_name = ZIP_TEMPLATE % str(archive_num)  
+  archive_path = os.path.join(util.DATA, archive_name)
   with zipfile.ZipFile(archive_path) as zf:
-    # first entry is like '<archive_num>/'
-    pages = zf.namelist()[1:]          
-    if whitelist is not None:
-      pages = [page for page in pages if page.split('/')[1] in whitelist]
+    # first entry is '<archive_num>/'
+    pages = zf.namelist()[1:]
+    if train_data is not None:
+      pages = [page for page in pages if page.split('/')[1] in train_data]
     for page in pages:
       f = zf.open(page)
       soup = bs(f, 'lxml')
-      yield soup
+      page_name = page.split('/')[1]
+      if train_data is not None:
+        yield (train_data[page_name], soup)
+      else:
+        yield (page_name, soup)
+        
+
+
+
