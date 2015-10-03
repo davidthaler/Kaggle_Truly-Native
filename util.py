b7d2907ff79d8fd40c58f0a5f91a348c371135f1
David Thaler <davidthaler@gmail.com>
2015-10-02 21:47:28 -0700

    force labels to python int from numpy.int64 so that we can use output with pypy

--- util.py
+++ util.py
@@ -7,6 +7,8 @@ import paths
 def load_train(as_dict):
   '''
   Loads full training set, including labels, as either a data frame or a dict.
+  Rewrites label to a python int (it comes in as numpy.int64) so that we can 
+  use this dict with pypy, if as_dict is True.
   
   Args:
     as_dict - If True, return a dict from filenames to labels
@@ -18,7 +20,9 @@ def load_train(as_dict):
   '''
   tr = pd.read_csv(paths.TRAIN)
   if as_dict:
-    return dict(zip(tr.file, tr.sponsored))
+    # prevents numpy.int64 from getting into the dict and killing pypy
+    labels = [int(x) for x in tr.sponsored]
+    return dict(zip(tr.file, labels))
   else:
     return tr
 
@@ -41,7 +45,9 @@ def sample_submission():
 def create_sample(outfile, n_pos, n_neg):
   '''
   Creates a dict describing a specific sample of rows and saves 
-  it at ARTIFACTS with form {filename : label}.
+  it at ARTIFACTS with form {filename : label}. Rewrites label 
+  to a python int (it comes in as numpy.int64) so that we can 
+  use this dict with pypy.
   
   args:
     outfile - the dict is written at ARTIFACTS/<outfile>.pkl
@@ -57,7 +63,10 @@ def create_sample(outfile, n_pos, n_neg):
   tr_neg = tr[tr.sponsored == 0]
   tr_neg = tr.sample(n_neg)
   sample_df = tr_pos.append(tr_neg)
-  sample = dict(zip(sample_df.file, sample_df.sponsored))
+  # We need this to prevent the pickled sample dict from containing a 
+  # numpy.int64, which prevents using pypy
+  labels = [int(x) for x in sample_df.sponsored]
+  sample = dict(zip(sample_df.file, labels))
   put_artifact(sample, outfile)
 
 
