653f045e90d4dc4f9fcd64c40ea8513f8da6be64
David Thaler <davidthaler@gmail.com>
2015-10-07 20:11:24 -0700

    added script features to features.py

--- tree_features.py
+++ tree_features.py
@@ -3,6 +3,7 @@ from csv import DictWriter
 import os
 import argparse
 from datetime import datetime
+from urlparse import urlparse
 import zip_io
 import paths
 import artifacts
@@ -48,6 +49,29 @@ TEXT_NAMES = ['bracket_ct', 'long_doctype', 'markup_len', 'markup_line_ct',
               'semicolon_ct', 'short_doctype', 'strict', 'text_len', 
               'text_line_ct', 'text_token_ct', 'transitional']
 
+SCRIPT_FEATURES = ['script_text_ct', 'script_no_txt', 'total_script_length',
+                   'max_script_length', 'mean_script_length', 
+                   'script_tot_lines', 'script_max_lines', 
+                   'script_tot_braces', 'script_max_braces', 
+                   'script_urls', 'script_distinct_urls', 
+                   'script_src_wp_content', 'script_src_wp_includes', 
+                   'script_src_facebook', 'script_src_google',
+                   'script_src_twitter', 'script_src_pinterest', 
+                   'script_src_linkedin', 'script_src_jquery', 
+                   'script_src_mootools', 'script_src_tumblr', 
+                   'script_src_maps', 'script_src_amazonaws', 
+                   'script_src_cloudfront', 'script_src_stumbleupon', 
+                   'script_src_addthis', 'script_src_outbrain', 
+                   'script_src_googlesyndication', 
+                   'script_url_facebook', 'script_url_google',
+                   'script_url_twitter', 'script_url_pinterest', 
+                   'script_url_linkedin', 'script_url_jquery', 
+                   'script_url_mootools', 'script_url_tumblr', 
+                   'script_url_maps', 'script_url_amazonaws', 
+                   'script_url_cloudfront', 'script_url_stumbleupon', 
+                   'script_url_addthis', 'script_url_outbrain', 
+                   'script_url_googlesyndication']
+
 
 def write_sample(sample_dict, outfile):
   start = datetime.now()
@@ -65,7 +89,7 @@ def write_sample(sample_dict, outfile):
   tag_attr_names = ['_'.join(s.split()) for s in TAG_ATTR]
   fieldnames.extend(tag_attr_names)
   fieldnames.extend(TEXT_NAMES)
-  
+  fieldnames.extend(SCRIPT_FEATURES)
   with open(outpath, 'w') as f_out:
     writer = DictWriter(f_out, fieldnames=fieldnames)
     writer.writeheader()
@@ -87,6 +111,7 @@ def write_sample(sample_dict, outfile):
       add_tag_attr_vals(row, page)
       add_tag_attr(row, page)
       text_features(row, page)
+      script_features(row, page)
       writer.writerow(row)
   finish = datetime.now()
   print 'Elapsed time: %d sec.' % (finish - start).seconds
@@ -126,6 +151,67 @@ def text_features(row, page):
   row['text_token_ct'] = len(text.split())
   row['semicolon_ct'] = text.count(';')
   row['bracket_ct'] = text.count('}')
+
+
+def script_features(row, page):
+  safemax = lambda l : 0 if len(l) == 0 else max(l)
+  scripts = [tag.get_text(strip=True) for tag in page.select('script')]
+  scripts = [s for s in scripts if len(s) > 0]
+  lengths = [len(tag) for tag in scripts]
+  row['script_text_ct'] = len(scripts)
+  row['script_no_txt'] = row['script'] - row['script_text_ct']
+  row['total_script_length'] = sum(lengths)
+  row['max_script_length'] = safemax(lengths)
+  if len(scripts) > 0:
+    row['mean_script_length'] = 1.0 * row['total_script_length'] / len(scripts)
+  else:
+    row['mean_script_length'] = 0
+  line_ct = [s.count(';') for s in scripts]
+  row['script_tot_lines'] = sum(line_ct)
+  row['script_max_lines'] = safemax(line_ct)
+  braces = [s.count('}') for s in scripts]
+  row['script_tot_braces'] = sum(braces)
+  row['script_max_braces'] = safemax(braces)
+  srcs = [tag['src'] for tag in page.select('script[src]')]
+  row['script_src_wp_content'] = sum(['wp-content' in s for s in srcs])
+  row['script_src_wp_includes'] = sum(['wp-includes' in s for s in srcs])
+  row['script_src_facebook'] = sum(['facebook' in s for s in srcs])
+  row['script_src_google'] = sum(['google' in s for s in srcs])
+  row['script_src_twitter'] = sum(['twitter' in s for s in srcs])
+  row['script_src_pinterest'] = sum(['pinterest' in s for s in srcs])
+  row['script_src_linkedin'] = sum(['linkedin' in s for s in srcs])
+  row['script_src_jquery'] = sum(['jquery' in s for s in srcs])
+  row['script_src_mootools'] = sum(['mootools' in s for s in srcs])
+  row['script_src_tumblr'] = sum(['tumblr' in s for s in srcs])
+  row['script_src_maps'] = sum(['maps.google' in s for s in srcs])
+  row['script_src_amazonaws'] = sum(['amazonaws' in s for s in srcs])
+  row['script_src_cloudfront'] = sum(['cloudfront' in s for s in srcs])
+  row['script_src_stumbleupon'] = sum(['stumbleupon' in s for s in srcs])
+  row['script_src_addthis'] = sum(['addthis' in s for s in srcs])
+  row['script_src_outbrain'] = sum(['outbrain' in s for s in srcs])
+  row['script_src_googlesyndication'] = sum(['googlesyndication' in s for s in srcs])
+  urls = [urlparse(s).netloc for s in srcs]
+  urls = [u for u in urls if len(u) > 0]
+  row['script_urls'] = len(urls)
+  row['script_distinct_urls'] = len(set(urls))
+  row['script_url_facebook'] = sum(['facebook' in s for s in urls])
+  row['script_url_google'] = sum(['google' in s for s in urls])
+  row['script_url_twitter'] = sum(['twitter' in s for s in urls])
+  row['script_url_pinterest'] = sum(['pinterest' in s for s in urls])
+  row['script_url_linkedin'] = sum(['linkedin' in s for s in urls])
+  row['script_url_jquery'] = sum(['jquery' in s for s in urls])
+  row['script_url_mootools'] = sum(['mootools' in s for s in urls])
+  row['script_url_tumblr'] = sum(['tumblr' in s for s in urls])
+  row['script_url_maps'] = sum(['maps.google' in s for s in urls])
+  row['script_url_amazonaws'] = sum(['amazonaws' in s for s in urls])
+  row['script_url_cloudfront'] = sum(['cloudfront' in s for s in urls])
+  row['script_url_stumbleupon'] = sum(['stumbleupon' in s for s in urls])
+  row['script_url_addthis'] = sum(['addthis' in s for s in urls])
+  row['script_url_outbrain'] = sum(['outbrain' in s for s in urls])
+  row['script_url_googlesyndication'] = sum(['googlesyndication' in s for s in urls])
+  
+# also taboola, modernizr, optimizely prototype, scriptaculous, cloudflare,
+# bootstrap, plugins, themes...so we need a loop
 
 
 if __name__ == '__main__':
